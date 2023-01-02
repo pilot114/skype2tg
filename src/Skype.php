02d88cf6d607831d022e3bb74c2b760e41ef8abe
Oleg <chulkov@sibvaleo.com>
2023-01-03 00:23:16 +0200

    try auth flow

--- src/Skype.php
+++ src/Skype.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client;
 class Skype
 {
     protected string $entryPoint;
+    protected int $subscriptionId = 0;
     protected Client $client;
     protected array $options = [];
     protected bool $isDebug = false;
@@ -28,8 +29,10 @@ class Skype
         $this->client = new Client([
             'base_uri' => 'https://azeus1-client-s.gateway.messenger.live.com',
             'timeout'  => 1.0,
-            // 'debug' => true,
+//             'debug' => true,
         ]);
+
+//        $this->auth();
     }
 
     public function setDebug(bool $isDebug)
@@ -52,11 +55,10 @@ class Skype
     {
         $this->options['timeout'] = 90.0;
         $id = 1000;
-        $subscriptionId = 1;
 
         $eventIndex = 0;
         while($eventIndex < $count) {
-            $url = "/v1/users/ME/endpoints/$this->entryPoint/subscriptions/$subscriptionId/poll?ackId=$id";
+            $url = "/v1/users/ME/endpoints/$this->entryPoint/subscriptions/$this->subscriptionId/poll?ackId=$id";
             $events = $this->callPost($url);
             foreach ($events as $event) {
                 $id = $event['id'];
@@ -76,5 +78,88 @@ class Skype
         $response = $this->client->post($url, $this->options);
         $data = $response->getBody()->getContents();
         return json_decode($data, true)['eventMessages'] ?? [];
+    }
+
+    protected function auth(): bool
+    {
+        $skypeToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSaXhuQ25zdE5PU2p2cyIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzI2OTUxNzgsImV4cCI6MTY3Mjc4MTU3Nywic2t5cGVpZCI6InNldmVyYXNzZSIsInNjcCI6OTU4LCJjc2kiOiIxNjcyNjk1MTc3IiwiY2lkIjoiM2I0MjAzODFmYWQ4Nzc5MSIsImFhdCI6MTY3MjY3NjY3NX0.LJaXQzdN0wjPSFlKin_skDxPfsqa4MlW3y4tacMdfb3C2_F7MfYgBlgwAdZ-6LNoRN-CvUrP6HeOMNapWk9DiFT7uVC9TqAN5BVY29wAItar6HxYRna_ZZYndQ-xnE3i3Fwrlw3ZVFKZ3bjZMRrshLQq_B2jGS9YSYAL9HXYwnRB1TCwNF6nt2FQuLpbirAOCs7_WYIA4dBuNPEP6RzW2cJvAIjMyrFXjVlnyndGrwaXb3xpmiTffC2Hr-trG30uuGIbftq6cgdI-OcNUVIDlSetEEBgoNgtWJ2XBLrUk9060F0peIXI56dvmYg1XH2UBcR2wIkSuZMRkFv5ZRYnfg';
+
+        // 1 getCookies
+        $url = 'https://api.asm.skype.com/v1/skypetokenauth';
+        $options = [
+            'headers' => [
+                'Authorization' => "skype_token $skypeToken",
+            ],
+            'form_params' => [
+                'skypetoken' => $skypeToken,
+            ],
+        ];
+        $response = $this->client->post($url, $options);
+        $this->cookieForApi = $response->getHeader('Set-Cookie')[0] ?? '';
+
+        // 2 ?
+//        $url = 'https://edge.skype.com/trap/tokens';
+//        $options = [
+//            'headers' => [
+//                'x-skypetoken' => $skypeToken,
+//            ],
+//        ];
+//        $response = $this->client->get($url, $options);
+//        $tokens = $response->getBody()->getContents();
+//        dump($tokens);
+
+        // 3 getRegToken
+        $url = 'https://client-s.gateway.messenger.live.com/v1/users/ME/properties';
+        $options = [
+            'headers' => [
+                'Authentication' => "skypetoken=$skypeToken",
+            ],
+        ];
+        $response = $this->client->get($url, $options);
+        $regTokenString = $response->getHeader('Set-RegistrationToken')[0];
+//        preg_match('#registrationToken=(.*);#', $regTokenString, $matches);
+//        $regToken = $matches[1];
+        $this->endpointHeader = $regTokenString;
+
+        // 4 getEndpoint
+        $url = 'https://client-s.gateway.messenger.live.com/v1/users/ME/endpoints';
+        $options = [
+            'headers' => [
+                'Authentication' => "skypetoken=$skypeToken",
+                'RegistrationToken' => $regTokenString,
+            ],
+            'json' => [
+                'endpointFeatures' => 'Agent,Presence2015,MessageProperties,CustomUserProperties,Casts,ModernBots,AutoIdleForWebApi,secureThreads,notificationStream,InviteFree,SupportsReadReceipts',
+            ],
+//            'allow_redirects' => false,
+        ];
+        $response = $this->client->post($url, $options);
+        $endpoints = json_decode($response->getBody()->getContents(), true);
+        $endpoint = array_filter($endpoints, fn($x) => $x['isActive'] === true)[0] ?? null;
+
+        $endpointId = $endpoint['id'];
+        $subscriptionId = $endpoint['subscriptions'][0]['id'] ?? 0;
+
+        if (!$endpointId) {
+            dump('fail');
+            die();
+        }
+
+        $this->authHeader = "skypetoken=$skypeToken";
+        $this->endpointHeader .= sprintf('; endpointId={%s}', $endpointId);
+        $this->entryPoint = sprintf("{%s}", $endpointId);
+        $this->subscriptionId = $subscriptionId;
+
+        $this->options['headers'] = [
+            'Authentication' => "skypetoken=$skypeToken",
+            'RegistrationToken' => $this->endpointHeader,
+        ];
+        $this->options['timeout'] = 90.0;
+
+        foreach ($this->listenEvents(1000) as $event) {
+            dump($event);
+        }
+
+        return true;
     }
 }
